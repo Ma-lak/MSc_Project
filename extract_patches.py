bb04c31180dd6db96dd5a502fdfbe69fffd5b318
Malak <151854615+Ma-lak@users.noreply.github.com>
2026-07-22 19:09:41 +0100

    Feat: Update patch size to 200

--- extract_patches.py
+++ extract_patches.py
@@ -284,7 +284,7 @@ for epoch in range(epochs):
     print(f"Test reg: {test_reg:.4f}, class: {test_class:.4f}")
 
     if epoch % 10 == 0:
-        torch.save(model.state_dict(), f"model_s2_size70__128_patches_batch40_50epochs_checkpoint_epoch_{epoch}.pth")
+        torch.save(model.state_dict(), f"model_s2_size200__128_patches_50epochs_checkpoint_epoch_{epoch}.pth")
 
 
     # plt.draw()
@@ -317,7 +317,7 @@ plt.ylabel("Loss")
 plt.title("Training and Test Loss")
 plt.legend()
 plt.grid(True)
-plt.savefig("loss_plot__size70_128patches_batch40_50_epochs_s2.png") 
+plt.savefig("loss_plot__size200_128patches_batch40_50_epochs_s2.png") 
 
 #plt.show()
 print("DONE")
